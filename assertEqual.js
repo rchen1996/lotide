f1306bbe4ccabc737802ba4610ba45b4458c7f15
Rebecca Chen <rebecca061996@gmail.com>
2021-01-04 21:38:45 +0000

    Created an assertEqual function to determine whether 2 given inputs are strictly equal.

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ ✅ ✅ Assertion Passed: [" + actual + "] === [" + expected + "]");
+  } else {
+    console.log("🛑 🛑 🛑 Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  }
+};
+
+/* Test Code
+assertEqual("Yes", "Yes");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(5, 28);
+*/
