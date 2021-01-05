1cd03079d7812ef65b07af6980010e0d946f7a4a
Rebecca Chen <rebecca061996@gmail.com>
2021-01-05 21:44:07 +0000

    Created an eqArrays function to check if two input arrays are identical

---
+++ eqArrays.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+// function takes in 2 arrays and returns true/false based on perfect match
+const eqArrays = function(arr1, arr2) {
+  // need to keep track of whether or not the two are the same
+  let comparison;
+  // need to check if arrays are the same length
+  if (arr1.length !== arr2.length) {
+    comparison = false;
+    return comparison;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        comparison = true;
+      } else {
+        comparison = false;
+        return comparison;
+      }
+    }
+  }
+  return comparison;
+};
+
+// Test Cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1]), false);
+assertEqual(eqArrays([1], [1, 2]), false);
