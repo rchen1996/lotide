9865db0aa3bdce355a0f7f40a83a91a2fce9fd55
Rebecca Chen <rebecca061996@gmail.com>
2021-01-05 23:30:08 +0000

    modified function so that it leverages the previously implemented function rather than re-writing all the logic

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -29,28 +29,33 @@ const assertEqual = function(actual, expected) {
 // takes in 2 arrays and console.log a message
 // message will be similar to assertEqual
 
+
 const assertArraysEqual = function(arr1, arr2) {
-  let comparison;
+  // let comparison;
   // need to check if arrays are the same length
-  if (arr1.length !== arr2.length) {
-    comparison = false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        comparison = false;
-        i = arr1.length - 1;
-      } else {
-        comparison = true;
-      }
-    }
-  }
-  if (comparison === false) {
-    console.log(`🛑 🛑 🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
-  } else {
+  // if (arr1.length !== arr2.length) {
+  //   comparison = false;
+  // } else {
+  //   for (let i = 0; i < arr1.length; i++) {
+  //     if (arr1[i] !== arr2[i]) {
+  //       comparison = false;
+  //       i = arr1.length - 1;
+  //     } else {
+  //       comparison = true;
+  //     }
+  //   }
+  // }
+  // if (comparison === false) {
+  //   console.log(`🛑 🛑 🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  // } else {
+  //   console.log(`✅ ✅ ✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  // }
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅ ✅ ✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
   }
 };
-
 
 //Test Cases
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // should be true
