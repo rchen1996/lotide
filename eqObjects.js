64116641fc0d1728a7e7af6136a2f9d24b3c7bd0
Rebecca Chen <rebecca061996@gmail.com>
2021-01-06 20:22:00 +0000

    removed some unnecessary code

--- eqObjects.js
+++ eqObjects.js
@@ -9,27 +9,22 @@ const assertEqual = function(actual, expected) {
 // function takes in 2 objects
 // returns true or false based on a perfect match
 const eqObjects = function(obj1, obj2) {
-  // need a variable to check
-  let match;
   // objects need to have the same number of keys
   let obj1Keys = Object.keys(obj1); // array of keys for obj1
   let obj2Keys = Object.keys(obj2); // array of keys for obj2
   if (obj1Keys.length !== obj2Keys.length) {
     // if not same number of keys, auto fails
-    match = false;
-    return match;
+    return false;
   } else { // value for each key has to be the same for both objects
     for (const key of obj1Keys) {
       if (obj1[key] !== obj2[key]) {
-        match = false;
-        return match;
-      } else {
-        match = true;
+        return false;
       }
     }
+    // if all key-value pairs of the same, false is never returned
+    // so they must all be the same, return true at end of loop
+    return true;
   }
-  // catch all for when match = true;
-  return match;
 };
 
 // Test Cases
