1979c16d6b68aecb635ca40bb5abcf2f8875ba35
Rebecca Chen <rebecca061996@gmail.com>
2021-01-06 19:25:56 +0000

    initial code for function implemented

---
+++ letterPositions.js
@@ -0,0 +1,8 @@
+// return all the indices (zero-based positions) in the string where each character is found
+// multiple numbers may be needed to represent all the places
+const letterPositions = function(sentence) {
+  // need an empty obj to return
+  const results = {};
+  // logic to update results
+  return results;
+};
