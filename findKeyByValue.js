a7b259e05dcdfa5f1ecc5b3f1a8e7b0d8d8a7d51
Rebecca Chen <rebecca061996@gmail.com>
2021-01-06 19:57:18 +0000

    completed function logic for findKeyByValue function such that it returns the first key where the input value is found as the value of the key-value pair

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -10,12 +10,22 @@ const assertEqual = function(actual, expected) {
 // scan object and return the first key which contains the given value
 // if no key with given value is found, return undefined
 const findKeyByValue = function(obj, value) {
-
+  // need to loop through the obj and look at the values
+  // check if the value of each key is the same as input value
+  // need to loop through the keys of the object - done by creating an array of keys using Object.keys()
+  for (let key of Object.keys(obj)) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  // if the key doesn't exist, then nothing will be returned from the loop
+  // need a catch all
+  return undefined;
 };
 
 // Test Cases
 const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
+  "sci_fi": "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama: "The Wire"
 };
