a9cebcee6c5ea7af5f55b004da73793ca6df03cd
Rebecca Chen <rebecca061996@gmail.com>
2021-01-06 20:19:34 +0000

    Function logic for eqObjects when dealing with primitives as values

--- eqObjects.js
+++ eqObjects.js
@@ -12,20 +12,30 @@ const eqObjects = function(obj1, obj2) {
   // need a variable to check
   let match;
   // objects need to have the same number of keys
-  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+  let obj1Keys = Object.keys(obj1); // array of keys for obj1
+  let obj2Keys = Object.keys(obj2); // array of keys for obj2
+  if (obj1Keys.length !== obj2Keys.length) {
     // if not same number of keys, auto fails
     match = false;
     return match;
+  } else { // value for each key has to be the same for both objects
+    for (const key of obj1Keys) {
+      if (obj1[key] !== obj2[key]) {
+        match = false;
+        return match;
+      } else {
+        match = true;
+      }
+    }
   }
-  // value for each key has to be the same for both objects
+  // catch all for when match = true;
+  return match;
 };
 
 // Test Cases
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // should return true
-assertEqual(eqObjects(ab, ba), ab); // should fail
+assertEqual(eqObjects(ab, ba), true); // should pass
 
 const abc = {a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // should return false
-assertEqual(eqObjects(ab, abc), ab); // should fail
+assertEqual(eqObjects(ab, abc), false); // should pass
