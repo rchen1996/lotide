6831557765d5a07b6e978d677cae51d0c138157d
Rebecca Chen <rebecca061996@gmail.com>
2021-01-06 19:16:42 +0000

    Added initial code for the countLetters function with comments about how to proceed

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,14 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑 🛑 🛑 Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
+
+// takes in a string and returns a count of each letter in the sentence
+// don't need to count spaces
+const countLetters = function(str) {
+  // need an empty obj to return later
+  const lettersCounted = {};
+  // need to loop through the string
+  // need to add to a count at each letter
+  // if property doesn't exist, need to create the count for it first before adding
+  return lettersCounted;
+};
