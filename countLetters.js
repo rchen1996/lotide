25cf757dcc10c14ace56e21edf97abf574b0b953
Rebecca Chen <rebecca061996@gmail.com>
2021-01-06 19:21:49 +0000

    Completed countLetters function such that it returns an object when a string a passed that counts how many of each letter is present in the string

--- countLetters.js
+++ countLetters.js
@@ -14,5 +14,19 @@ const countLetters = function(str) {
   // need to loop through the string
   // need to add to a count at each letter
   // if property doesn't exist, need to create the count for it first before adding
+  for (const letter of str) {
+    // check if the property already exists
+    if (lettersCounted[letter]) {
+      lettersCounted[letter] += 1;
+    } else {
+      lettersCounted[letter] = 1;
+    }
+  }
   return lettersCounted;
 };
+
+// Test Cases
+const result1 = countLetters("lighthouse in the house");
+assertEqual(result1["l"], 1);
+assertEqual(result1["o"], 2);
+assertEqual(result1["h"], 4);
