5633edbffca523055160d1099d86f748b2785240
Rebecca Chen <rebecca061996@gmail.com>
2021-01-06 19:27:44 +0000

    added code for assertArraysEqual functions for testing purposes

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  // need to keep track of whether or not the two are the same
+  let comparison;
+  // need to check if arrays are the same length
+  if (arr1.length !== arr2.length) {
+    comparison = false;
+    return comparison;
+  } else {
+    if (arr1.length === 0) {
+      comparison = true;
+      return comparison;
+    } else {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === arr2[i]) {
+          comparison = true;
+        } else {
+          comparison = false;
+          return comparison;
+        }
+      }
+    }
+  }
+  return comparison;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
 // return all the indices (zero-based positions) in the string where each character is found
 // multiple numbers may be needed to represent all the places
 const letterPositions = function(sentence) {
