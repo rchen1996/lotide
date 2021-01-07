874951e6e382e4bdd3542285e21ead296ba0bc9c
Rebecca Chen <rebecca061996@gmail.com>
2021-01-07 20:16:54 +0000

    added assertion test with multiple test cases

--- map.js
+++ map.js
@@ -1,3 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  // need to keep track of whether or not the two are the same
+  let comparison;
+  // need to check if arrays are the same length
+  if (arr1.length !== arr2.length) {
+    comparison = false;
+    return comparison;
+  } else {
+    if (arr1.length === 0) {
+      comparison = true;
+      return comparison;
+    } else {
+      for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === arr2[i]) {
+          comparison = true;
+        } else {
+          comparison = false;
+          return comparison;
+        }
+      }
+    }
+  }
+  return comparison;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ ✅ ✅ Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`🛑 🛑 🛑 Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
 // map function takes 2 arguments: array to map, callback function
 // function returns new array based on results of callback function
 
@@ -16,4 +49,11 @@ const map = function(array, callback) {
 const words = ["ground", "control", "to", "major", "tom"];
 
 const results1 = map(words, word => word[0]);
-console.log(results1);
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+
+const results2 = map(words, word => word + "s");
+assertArraysEqual(results2, ["grounds", "controls", "tos", "majors", "toms"]);
+
+const numbs = [1, 2, 3, 4, 5];
+const results3 = map(numbs, numb => numb + 1);
+assertArraysEqual(results3, [2, 3, 4, 5, 6]);
