1e86ab88a98dbbb127a4c5fa6b13840bbb3a5cc7
Rebecca Chen <rebecca061996@gmail.com>
2021-01-07 20:38:07 +0000

    completed logic for takeUntil function such that it will create a new array with elements from the beginning of the original array until a condition from a callback function is met

--- takeUntil.js
+++ takeUntil.js
@@ -37,7 +37,16 @@ const assertArraysEqual = function(arr1, arr2) {
 // callback should only be provided the item in the array
 // once it takes in a truthy value, the loop should stop
 const takeUntil = function(array, callback) {
-
+  // need it to return an array
+  let result = [];
+  for (let item of array) { // loop through the array
+    if (callback(item)) {
+      break; // breaks out of the loop once truthy
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
 };
 
 // Test cases
