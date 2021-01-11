90c635e7bb53cbf71f6cc006319cd89978cf24a4
Rebecca Chen <rebecca061996@gmail.com>
2021-01-11 17:30:28 +0000

    refactored code to export function and removed test cases

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑 🛑 🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 // function should not modify array that is passed in
 // should return a new array
 const tail = function(arr) {
@@ -16,16 +8,4 @@ const tail = function(arr) {
   return arrTails;
 };
 
-// Test case: Check the original array to make sure it hasn't been modified
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const resultWords = tail(words);
-assertEqual(words.length, 3); // original array should still have 3 elements
-assertEqual(resultWords[1], "Labs");
-
-// Test case: array with 1 element
-const one = [3];
-console.log(tail(one));
-
-//Test case: empty array
-const empty = [];
-console.log(tail(empty));
+module.exports = tail;
