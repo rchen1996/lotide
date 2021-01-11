feb561e9546386ce8d544572738dcc9d9d71c698
Rebecca Chen <rebecca061996@gmail.com>
2021-01-11 18:22:57 +0000

    refactored to use M & C

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const { assert } = require('chai');
 const middle = require('../middle');
 
-// Test Code
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(["1"]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, "2", "3", 3]), ["2", "3"]);
+describe('#middle', () => {
+  it('should return [] for [1, 2]', () => {
+    assert.deepStrictEqual(middle([1, 2]), []);
+  });
+
+  it('should return [] for ["1"]', () => {
+    assert.deepStrictEqual(middle(["1"]), []);
+  });
+
+  it('should return [2] for [1, 2, 3]', () => {
+    assert.deepStrictEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it('should return ["2", "3"] for [1, "2", "3", 3]', () => {
+    assert.deepStrictEqual(middle([1, "2", "3", 3]), ["2", "3"]);
+  });
+});
