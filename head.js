07d2e86e28acca686788d137ba0d237e5a198e39
Rebecca Chen <rebecca061996@gmail.com>
2021-01-11 17:23:18 +0000

    refactor to remove test cases and export function

--- head.js
+++ head.js
@@ -1,21 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑 🛑 🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 // function returns first item in the array
 // use assertEqual to write test cases
 const head = function(arr) {
   return arr[0];
 };
 
-/* Test cases
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([3]), 3);
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Cat", "Dog"]), "Purple");
-*/
+module.exports = head;
