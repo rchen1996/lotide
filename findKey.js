d7d39d87d0a1f47abe57f7d3dffebcf041cc7e8a
Rebecca Chen <rebecca061996@gmail.com>
2021-01-11 22:43:58 +0000

    moved test cases to own file

--- findKey.js
+++ findKey.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 // function takes in an object and a callback
 // scans the object and returns the first key for which the callback returns truthy
 // if no key found, return undefined
@@ -14,27 +12,3 @@ const findKey = function(obj, callback) {
 };
 
 module.exports = findKey;
-
-// // Test Case
-// const starsName = {
-//   "Blue Hill": {stars: 1},
-//   "Akaleri": {stars: 3},
-//   "noma": {stars: 2},
-//   "elBulli": {stars: 3},
-//   "Ora": {stars: 2},
-//   "Akelarre": {stars: 3}
-// };
-// let result1 = findKey(starsName, x => x.stars === 2); // "noma"
-// assertEqual(result1, "noma");
-// let result3 = findKey(starsName, x => x.stars > 4);
-// assertEqual(result3, undefined);
-// let result4 = findKey(starsName, x => x.stars === 3);
-// assertEqual(result4, "Akaleri");
-
-// const catAttributes = {
-//   hosico: "fluffy" ,
-//   nino: "orange",
-//   sakura: "fluffy"
-// };
-// let result2 = findKey(catAttributes, x => x === "fluffy");
-// assertEqual(result2, "hosico");
